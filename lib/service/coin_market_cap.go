11992f85b4e762c05cd5a1893d03eb8d3b3cd3ae
Marc S. Brooks <marc@mbrooks.info>
2020-09-27 20:26:58 -0700

    Prework to support static configured endpoints

---
+++ lib/service/coin_market_cap.go
@@ -0,0 +1,41 @@
+//
+//  go-crypto-market-ui
+//  Command-line utility to track cryptocurrencies in realtime.
+//
+//  Copyright 2020, Marc S. Brooks (https://mbrooks.info)
+//  Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+
+package service
+
+import (
+	endpoint "github.com/nuxy/go-crypto-market-ui/lib/service/coin_market_cap"
+)
+
+//
+// Service provides endpoint methods.
+//
+type Service interface {
+	URL(endpointName string) string
+}
+
+//
+// CoinMarketCap declared data types.
+//
+type CoinMarketCap struct {}
+
+//
+// URL returns an unprocessed path.
+//
+func (CoinMarketCap) URL(endpointName string) string {
+	var rawURI string
+
+	switch endpointName {
+		case "Quotes":
+			rawURI = (endpoint.Quotes{}).URI()
+			break
+	}
+
+	return "https://pro-api.coinmarketcap.com/v1/" + rawURI + "&CMC_PRO_API_KEY=%s"
+}
---
+++ lib/service/coin_market_cap/quotes.go
@@ -0,0 +1,29 @@
+//
+//  go-crypto-market-ui
+//  Command-line utility to track cryptocurrencies in realtime.
+//
+//  Copyright 2020, Marc S. Brooks (https://mbrooks.info)
+//  Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+
+package coinmarketcap
+
+//
+// Endpoint provides runtime methods.
+//
+type Endpoint interface {
+	URI() string
+}
+
+//
+// Quotes declared data types.
+//
+type Quotes struct{}
+
+//
+// URI returns an unprocessed resource.
+//
+func (Quotes) URI() string {
+	return "cryptocurrency/quotes/latest?symbol=%s"
+}
