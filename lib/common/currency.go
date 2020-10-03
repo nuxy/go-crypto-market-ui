e23599098ef64bdcb98341486b7aaf99e2015058
Marc S. Brooks <marc@mbrooks.info>
2020-10-03 14:19:33 -0700

    Added currency display format helper

---
+++ lib/common/currency.go
@@ -0,0 +1,71 @@
+//
+//  go-crypto-market-ui
+//  Command-line utility to track cryptocurrencies in realtime.
+//
+//  Copyright 2020, Marc S. Brooks (https://mbrooks.info)
+//  Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+
+package common
+
+import (
+	"log"
+	"strings"
+
+	"github.com/leekchan/accounting"
+)
+
+//
+// Currency declared data types.
+//
+type Currency struct {
+	Code   string
+	Symbol string
+}
+
+//
+// NewCurrency creates a new currency instance.
+//
+func NewCurrency(code string) *Currency {
+	currency := &Currency{}
+	currency.Code = strings.ToUpper(code)
+	return currency
+}
+
+//
+// IsValid checks the currency code is supported.
+//
+func (currency *Currency) IsValid() bool {
+	var _, ok = accounting.LocaleInfo[currency.Code]
+	return ok
+}
+
+//
+// Format returns formatted currency value.
+//
+func (currency *Currency) Format(v float64) string {
+	symbol := currency.load()
+
+	ac := accounting.Accounting{
+		Symbol:    symbol,
+		Precision: 2,
+	}
+
+	return ac.FormatMoney(v)
+}
+
+//
+// Returns the symbol for a given currency code.
+//
+func (currency *Currency) load() string {
+	var symbol string
+
+	if currency.IsValid() {
+		symbol = accounting.LocaleInfo[currency.Code].ComSymbol
+	} else {
+		log.Fatal("Unsupported currency: ", currency.Code)
+	}
+
+	return symbol
+}
