a9677554a79504faa49dcd0f062b91c20dca8c44
Marc S. Brooks <marc@mbrooks.info>
2020-11-22 18:45:51 -0800

    Added Error widget and config validation

---
+++ lib/widgets/error.go
@@ -0,0 +1,73 @@
+//
+//  go-crypto-market-ui
+//  Command-line utility to track cryptocurrencies in realtime.
+//
+//  Copyright 2020, Marc S. Brooks (https://mbrooks.info)
+//  Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+
+package widgets
+
+import (
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+
+	"github.com/nuxy/go-crypto-market-ui/lib/common"
+)
+
+// Widget properties.
+var errorRect = common.Widget{
+	Left:   58,
+	Top:    4,
+	Right:  119,
+	Bottom: 9,
+}
+
+//
+// Error declared data types.
+//
+type Error struct {
+	Language *common.Language
+	content  string
+}
+
+//
+// NewError creates a new widget instance.
+//
+func NewError(language *common.Language) *Error {
+	widget := &Error{}
+	widget.Language = language
+	return widget
+}
+
+//
+// Render the widget.
+//
+func (widget *Error) Render() {
+	obj := widgets.NewParagraph()
+	obj.Text          = common.PadRgt("⚠", 2) + widget.content
+	obj.BorderStyle   = common.WidgetBorderStyle()
+	obj.TitleStyle    = common.WidgetTitleStyle()
+	obj.PaddingLeft   = 1
+	obj.PaddingTop    = 1
+	obj.PaddingRight  = 1
+	obj.PaddingBottom = 1
+
+	obj.SetRect(
+		errorRect.Left,
+		errorRect.Top,
+		errorRect.Right,
+		errorRect.Bottom,
+	)
+
+	ui.Render(obj)
+}
+
+//
+// Message defines the instance content.
+//
+func (widget *Error) Message(v string) *Error {
+	widget.content = v
+	return widget
+}
--- terminal.go
+++ terminal.go
@@ -28,6 +28,7 @@ type Terminal struct {
 	Config    *lib.Config
 	Currency  *common.Currency
 	Language  *common.Language
+	Error     string
 	useTicker bool
 }
 
@@ -60,12 +61,36 @@ func (terminal *Terminal) initTermui() {
 
 	defer ui.Close()
 
-	// Render widgets.
-	if terminal.Config.IsValid() {
-		terminal.renderDashboard()
-	} else {
+	// Validate required fields.
+	if !terminal.Config.IsValid() {
+		terminal.Error = terminal.Language.Translate("MissingValues")
+
 		terminal.renderSetup()
 	}
+
+	if len(terminal.Config.ServiceName()) == 0 {
+		terminal.errors = append(
+			terminal.errors,
+			terminal.Language.Translate("InvalidService"),
+		)
+	}
+
+	if !terminal.Currency.IsValid() {
+		terminal.Error = terminal.Language.Translate("InvalidCurrency")
+
+		terminal.renderSetup()
+	}
+
+	if !terminal.Language.IsValid() {
+		terminal.Error = terminal.Language.Translate("InvalidLanguage")
+
+		terminal.renderSetup()
+	}
+
+	terminal.Error = ``
+
+	// Render termui widgets.
+	terminal.renderDashboard()
 }
 
 //
@@ -159,14 +184,19 @@ func (terminal *Terminal) renderHelp() {
 func (terminal *Terminal) renderSetup() {
 	terminal.useTicker = false
 
-	widget := terminal.initSetup()
+	widget1 := terminal.initSetup()
+	widget2 := terminal.initError()
 
 	actions := func() {
-		widget.Render()
+		widget1.Render()
+
+		if len(terminal.Error) > 0 {
+			widget2.Message(terminal.Error).Render()
+		}
 	}
 
 	events := func(e ui.Event) {
-		widget.Events(e)
+		widget1.Events(e)
 	}
 
 	actions()
@@ -179,6 +209,13 @@ func (terminal *Terminal) renderSetup() {
 //
 func (terminal *Terminal) initClock() *widgets.Clock {
 	return widgets.NewClock(terminal.Language)
+}
+
+//
+// Returns an instance of the Error widget.
+//
+func (terminal *Terminal) initError() *widgets.Error {
+	return widgets.NewError(terminal.Language)
 }
 
 //
